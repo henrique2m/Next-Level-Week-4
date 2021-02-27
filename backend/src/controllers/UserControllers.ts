c6e604a52d0ae0415ff06ced97b74a2226bf5fab
henrique2m <btdavintehenrique@gmail.com>
2021-02-26 23:11:03 -0300

    chore: :sparkles: automated testing :rocket:

---
+++ backend/src/controllers/UserControllers.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UserController {
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
+
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userAlreadyExits = await usersRepository.findOne({
+      email,
+    });
+
+    if (userAlreadyExits) {
+      return response.status(400).json({
+        error: "User Already exists!",
+      });
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+    });
+
+    await usersRepository.save(user);
+
+    return response.status(201).json(user);
+  }
+}
+
+export { UserController };
